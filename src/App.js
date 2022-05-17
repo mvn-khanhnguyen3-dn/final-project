9d81ad5da84f441a54138c1ee471227f82e62964
Khanh <nguyenquockhanhkz20@gmail.com>
2022-05-17 15:35:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Header, Footer } from "./components/layouts";
 import { Home, Login, Account } from "./pages";
 import PrivateRoute from "./core/guards/PrivateRouter";
-import { Spin } from "antd";
-const Feature = React.lazy(() => import("./pages/Feature"));
+const Feature = lazy(() => import("./pages/Feature"));
 
 function App() {
   return (
     <>
-      <Suspense fallback={<Spin />}>
-          <Router>
-            <Header />
-            <Switch>
-              <Route path="/product">
-                <Feature />
-              </Route>
-              <PrivateRoute path="/account">
-                <Account />
-              </PrivateRoute>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-            <Footer />
-          </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/product">
+              <Feature />
+            </Route>
+            <PrivateRoute path="/account">
+              <Account />
+            </PrivateRoute>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
       </Suspense>
     </>
   );
